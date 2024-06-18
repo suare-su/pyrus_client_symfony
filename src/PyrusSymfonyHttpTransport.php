ad010fbf5a91a87f74f4ef149fb66e719aa6e2c1
marvin255 <postbox433@gmail.com>
2024-06-18 22:24:46 +0200

    Add PyrusSymfonyHttpTransport

---
+++ src/PyrusSymfonyHttpTransport.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SuareSu\PyrusClientSymfony;
+
+use SuareSu\PyrusClient\Client\PyrusClientOptions;
+use SuareSu\PyrusClient\Transport\PyrusRequest;
+use SuareSu\PyrusClient\Transport\PyrusRequestMethod;
+use SuareSu\PyrusClient\Transport\PyrusResponse;
+use SuareSu\PyrusClient\Transport\PyrusResponseStatus;
+use SuareSu\PyrusClient\Transport\PyrusTransport;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+/**
+ * Facade for HTTP client.
+ *
+ * @psalm-api
+ */
+final class PyrusSymfonyHttpTransport implements PyrusTransport
+{
+    public function __construct(private HttpClientInterface $symfonyTransport)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @psalm-suppress MixedArrayAssignment
+     */
+    public function request(PyrusRequest $request, ?PyrusClientOptions $options = null): PyrusResponse
+    {
+        $symfonyOptions = [];
+
+        if (!empty($request->headers)) {
+            $symfonyOptions['headers'] = $request->headers;
+        }
+
+        if (PyrusRequestMethod::GET === $request->method) {
+            $symfonyOptions['query'] = $request->payload;
+        } else {
+            $symfonyOptions['body'] = $request->payload;
+        }
+
+        if (null !== $options) {
+            $symfonyOptions['max_duration'] = $options->timeout;
+            $symfonyOptions['retry_failed'] = [
+                'max_retries' => $options->maxRetries,
+                'delay' => $options->retryDelay,
+            ];
+        }
+
+        $response = $this->symfonyTransport->request(
+            $request->method->value,
+            $request->url,
+            $symfonyOptions
+        );
+
+        return new PyrusResponse(
+            PyrusResponseStatus::from($response->getStatusCode()),
+            $response->getContent()
+        );
+    }
+}
