54d5a180fe0ea8a3d2c343689cd9b87e0b31c2be
marvin255 <postbox433@gmail.com>
2024-12-14 13:52:22 +0100

    Add field converters

---
+++ tests/FormConverter/FieldConverter/PyrusFieldConverterCheckMarkTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SuareSu\PyrusClientSymfony\Tests\FormConverter\FieldConverter;
+
+use SuareSu\PyrusClient\Entity\Form\FormFieldType;
+use SuareSu\PyrusClientSymfony\FormConverter\FieldConverter\PyrusFieldConverterCheckMark;
+use SuareSu\PyrusClientSymfony\FormConverter\FieldConverter\PyrusFieldConverterHelper;
+use SuareSu\PyrusClientSymfony\Tests\BaseCasePyrusForm;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+/**
+ * @internal
+ */
+final class PyrusFieldConverterCheckMarkTest extends BaseCasePyrusForm
+{
+    /**
+     * @test
+     *
+     * @dataProvider provideSupportsConversion
+     */
+    public function testSupportsConversion(FormFieldType $type, bool $expected): void
+    {
+        $field = $this->createPyrusFieldMock(['type' => $type]);
+        $form = $this->createPyrusFormMock($field);
+
+        $converter = new PyrusFieldConverterCheckMark();
+        $res = $converter->supportsConversion($form, $field);
+
+        $this->assertSame($expected, $res);
+    }
+
+    public static function provideSupportsConversion(): array
+    {
+        return [
+            'number type' => [
+                FormFieldType::CHECKMARK,
+                true,
+            ],
+            'other types' => [
+                FormFieldType::TEXT,
+                false,
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function testConvert(): void
+    {
+        $field = $this->createPyrusFieldMock(
+            [
+                'type' => FormFieldType::CHECKMARK,
+            ]
+        );
+
+        $form = $this->createPyrusFormMock($field);
+
+        $builder = $this->createSymfonyFormBuilderMock();
+        $builder->expects($this->once())
+            ->method('add')
+            ->with(
+                $this->identicalTo(PyrusFieldConverterHelper::getHtmlName($field)),
+                $this->identicalTo(CheckboxType::class),
+                $this->identicalTo(PyrusFieldConverterHelper::getDefaultOptions($field)),
+            );
+
+        $converter = new PyrusFieldConverterCheckMark();
+        $converter->convert($form, $field, $builder);
+    }
+}
