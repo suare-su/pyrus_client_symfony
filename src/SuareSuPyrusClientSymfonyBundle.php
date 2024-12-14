844a8e58aadda6ef700c04dfe92212cb01880d55
marvin255 <postbox433@gmail.com>
2024-12-14 15:24:21 +0100

    Add form converter config

--- src/SuareSuPyrusClientSymfonyBundle.php
+++ src/SuareSuPyrusClientSymfonyBundle.php
@@ -24,7 +24,7 @@ class SuareSuPyrusClientSymfonyBundle extends AbstractBundle
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import(self::SERVICES_YAML);
-        if ($container->hasExtension('form')) {
+        if ($builder->hasExtension('form')) {
             $container->import(self::FORM_CONVERTER_SERVICES_YAML);
         }
     }
